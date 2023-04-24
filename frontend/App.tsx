5b43d4c26d14039c037efbcd55cf344d172bc2ae
Gustavo <gust4v0309@gmail.com>
2023-04-24 15:18:39 -0300

    Feat: Consume Imgtest component

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Imgtest } from './globalStyles';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Imgtest source={require('./src/assets/cat-logo.png')} />
     </View>
   );
 }
