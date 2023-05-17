2b56040002e01d86b8c70cfbbff20035f428d85d
Gustavo <gust4v0309@gmail.com>
2023-05-17 01:18:30 -0300

    Feat: create styles to IntroductionOne with styledComponents

---
+++ frontend/src/pages/IntroductionOne/styles.ts
@@ -0,0 +1,47 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: flex-start;
+  margin-top: 120px;
+`;
+
+export const IntroductionImage = styled.Image`
+  width: 400px;
+  height: 310px;
+`;
+
+export const BackgroundImage = styled.ImageBackground`
+  width: 439px;
+  height: 456px;
+`;
+
+export const ImageWrapper = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const ContainerLogo = styled.View`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 10px;
+`
+
+export const ImageLogo = styled.Image`
+    width: 36px;
+    height: 41px;
+`
+
+export const TextLogo = styled.Text`
+    font-weight: 700
+`
+
+export const TextIntroduction = styled.Text`
+    font-size: 18px;
+    font-weight: 500;
+    color: #8F8F8F;
+    margin-top: 10px;
+`
