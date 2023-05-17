06270343a2e7df40f64fdeadd69d41c6b00e8c26
Gustavo <gust4v0309@gmail.com>
2023-05-17 01:21:21 -0300

    Feat: Structuring IntroductionOne

--- frontend/src/pages/IntroductionOne/index.tsx
+++ frontend/src/pages/IntroductionOne/index.tsx
@@ -1,7 +1,20 @@
+import React from 'react';
+import { BackgroundImage, Container, IntroductionImage, ImageWrapper, ContainerLogo, ImageLogo, TextLogo, TextIntroduction } from "./styles";
+import { Text } from 'react-native';
+
 export default function IntroductionOne() {
-    return(
-        <div>
-            
-        </div>
-    )
+  return (
+    <Container>
+      <BackgroundImage source={require('../../assets/HomeScreens/green-elipse.svg')}>
+        <ImageWrapper>
+          <IntroductionImage source={require('../../assets/HomeScreens/introduction one.svg')} />
+        </ImageWrapper>
+      </BackgroundImage>
+      <ContainerLogo>
+        <ImageLogo source={require('../../assets/logo/logo black.svg')} />
+        <Text style={{fontSize: 35}}>Receite<TextLogo>.Me</TextLogo></Text>
+        <TextIntroduction>O seu app favorito de receitas!</TextIntroduction>
+      </ContainerLogo>
+    </Container>
+  );
 }
