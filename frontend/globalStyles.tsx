c0bc0374cd25e264f0e54091c68d19c490d6a1be
Gustavo <gust4v0309@gmail.com>
2023-05-19 00:57:56 -0300

    Feat: Create 'Weight700' in globalStyles.tsx

--- frontend/globalStyles.tsx
+++ frontend/globalStyles.tsx
@@ -1,6 +1,5 @@
 import styled from 'styled-components/native';
 
-export const Imgtest = styled.Image`
-    width: 100px;
-    height: 100px;
+export const Weight700 = styled.Text`
+    font-weight: 700;
 `
