40c905c5c14492ebab4e40cde4ff029aeebc9d8b
Gustavo <gust4v0309@gmail.com>
2023-05-31 15:20:31 -0300

    Feat: delete unnecessary components

--- frontend/src/components/Waves/index.tsx
+++
@@ -1,8 +0,0 @@
-import { Text, View } from "react-native";
-import { Container, Wave} from "./styles";
-
-export default function Waves() {
-    return (
-        <Wave source={require("../../assets/HomeScreens/wave.png")}/>
-      );
-}
