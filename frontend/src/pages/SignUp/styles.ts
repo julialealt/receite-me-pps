f4cf796139da89a190c619c0e7fc74cc66f109bc
Gustavo <gust4v0309@gmail.com>
2023-05-31 15:25:47 -0300

    Feat: SignUp Page styles

---
+++ frontend/src/pages/SignUp/styles.ts
@@ -0,0 +1,27 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+    flex: 1;
+    align-items: center;
+    margin: 75px 0;
+    gap: 17px;
+`
+
+export const LoginText = styled.Text`
+    font-size: 25px;
+    font-weight: 700;
+    color: #272727;
+    margin: 0 0 8px 0 ;
+`
+
+export const SubText = styled.Text`
+    font-size: 12px;
+    font-weight: 500;
+    color: #686868;
+`
+
+export const ContainerButton = styled.View`
+    align-items: center;
+    gap: 30px;
+    margin-top:13px;
+`
