4ed6bbc040627f38f21fe295bfbe873f5c9a4eec
Gustavo <gust4v0309@gmail.com>
2023-05-31 15:25:38 -0300

    Feat: create signUp Page

---
+++ frontend/src/pages/SignUp/index.tsx
@@ -0,0 +1,19 @@
+import { Weight700 } from "../../../globalStyles";
+import Button from "../../components/Button";
+import Input from "../../components/Input";
+import { Container, ContainerButton, LoginText, SubText } from "./styles";
+
+export default function SignUp() {
+    return(
+        <Container>
+            <LoginText>Sign up</LoginText>
+            <Input label="Nome" placeholder="João" />
+            <Input label="Email" placeholder="joao@gmail.com"/>
+            <Input label="Senha" placeholder="*********"/>
+            <ContainerButton>
+                <Button labelButton="CADASTRAR" width={290} height={47} radius={10} />
+                <SubText>Já possui uma conta ? <Weight700 style={{color: "green"}}>Entre agora</Weight700></SubText>
+            </ContainerButton>
+        </Container>
+    )
+}
