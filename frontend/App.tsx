53cc66c86cd65ed68405463e19d8b28764298250
Gustavo <gust4v0309@gmail.com>
2023-05-31 14:41:12 -0300

    Feat: App.tx

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,19 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import IntroductionOne from './src/pages/InitialSliders';
-import Waves from './src/components/Waves';
+import InitialSliders from './src/pages/InitialSliders';
+import Login from './src/pages/Login';
+import SignUp from './src/pages/SignUp';
 
 export default function App() {
   return (
-      <IntroductionOne />
+      <SignUp />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#FFFFFF',
-  },
-});
