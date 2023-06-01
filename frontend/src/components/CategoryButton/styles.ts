83925f6989ba22698ed93dff3d18866e7ebfd0ec
Gustavo <gust4v0309@gmail.com>
2023-06-01 16:10:52 -0300

    Feat: create styles to CategoryButton

---
+++ frontend/src/components/CategoryButton/styles.ts
@@ -0,0 +1,24 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: 70px;
+    height: 85px;
+    border: 1px solid #878787;
+    border-radius: 10px;
+
+`
+
+export const IconCategory = styled.Image`
+    width: 30px;
+    height: 30px;
+`
+
+export const TextCategory = styled.Text`
+    font-size: 10px;
+    font-weight: 600;
+
+`
