3e59ffd577e03fbfb1c442161f7edc205e8e7bed
Gustavo <gust4v0309@gmail.com>
2023-06-02 15:51:58 -0300

    Feat: create styles to SearchViewer

---
+++ frontend/src/pages/SearchViewer/styles.ts
@@ -0,0 +1,53 @@
+import styled from 'styled-components/native';
+
+//View
+
+export const Container = styled.View`
+    display: flex;
+    flex-direction: column;
+    gap: 30px;
+    margin-top: 64px;
+    padding: 0 30px;
+`
+export const UserContainer = styled.View`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+`
+
+export const GreetingUserContainer = styled.View`
+    display: flex;
+    flex-direction: column;
+    gap: 8px;
+`
+
+export const RecipeContainer = styled.View`
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: row;
+    justify-content: center;
+    gap: 20px;
+`;
+
+//Text
+
+export const GreetingText = styled.Text`
+    font-size: 14px;
+    font-weight: 600;
+    color: #a9a9a9;
+
+`
+
+export const TitleText = styled.Text `
+    font-size: 20px;
+    font-weight: 700;
+    width: 230px;
+`
+
+//Image
+
+export const UserLogo = styled.Image `
+    width: 100px;
+    height: 100px;
+`
