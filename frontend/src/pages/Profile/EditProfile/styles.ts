3a6fb300554abc43a02a2f4e600baba5413da226
Gustavo <gust4v0309@gmail.com>
2023-06-05 20:40:55 -0300

    Feat: create Styles to EditProfile

---
+++ frontend/src/pages/Profile/EditProfile/styles.ts
@@ -0,0 +1,25 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-top: 85px;
+    padding: 0  15px;
+`
+
+export const ContainerEditProfileInputs = styled.View`
+    display: flex;
+    flex-direction: column; 
+    width: 100%;
+    align-items: center;
+    gap: 17px;
+    margin: 40px 0;
+    
+`
+
+
+export const EditProfileText = styled.Text`
+    font-size: 20px;
+    font-weight: 700;
+`
