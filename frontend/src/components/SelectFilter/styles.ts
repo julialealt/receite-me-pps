fdad4040ff4bac585a459c9643230947fb8388d9
Gustavo <gust4v0309@gmail.com>
2023-06-05 20:40:32 -0300

    Feat: create styles to SelectFilter

---
+++ frontend/src/components/SelectFilter/styles.ts
@@ -0,0 +1,57 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-top: 100px;
+    width: 100%;
+    padding: 25px;
+`
+
+export const SelectButtonScope = styled.View`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    gap: 80px;
+    background: #F8F8F8;
+    padding: 24px;
+    border-top-left-radius: 25px ;
+    border-bottom-left-radius: ${(props: { isFocused: boolean; }) => (props.isFocused ? '0' : '25px')};
+    border-top-right-radius: 25px;
+    border-bottom-right-radius: ${(props: { isFocused: boolean; }) => (props.isFocused ? '0' : '25px')};
+    width: 100%;
+`
+
+export const SelectButtonImage = styled.Image`
+    width: 16px;
+    height: 26px;
+`
+
+export const SelectButtonTextInput = styled.TextInput`
+    font-size: 14px;
+    font-weight: 500;
+    color: #a9a9a9;
+    flex-grow: 1;
+`
+
+export const SelectButtonFlatList = styled.FlatList`
+    background: #F8F8F8;
+    width: 100%;
+    border-bottom-left-radius: 25px;
+    border-bottom-right-radius: 25px;
+`
+
+export const SelectButttonFlatListText = styled.Text`
+    display: flex;
+    align-self: center;
+    color: #A9A9A9; 
+    font-size: 14px;
+    font-weight: 500;
+    margin-bottom: 38px;
+`
+
+export const SelectButtonTouchableOpacity = styled.TouchableOpacity`
+    display: flex;
+    width: 50px;
+`
