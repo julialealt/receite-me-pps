f1182f4a580cfff438a3235a01536ee935f44627
Gustavo <gust4v0309@gmail.com>
2023-06-06 20:51:04 -0300

    Feat: create styles to SelectedIngredient

---
+++ frontend/src/components/SelectedIngredient/styles.ts
@@ -0,0 +1,28 @@
+import styled from 'styled-components/native';
+
+export const ScopeButton = styled.View`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+    padding: 20px 25px;
+    background: #F8F8F8;
+    border-radius: 15px;
+`
+
+export const TextButton = styled.Text`
+    font-size: 16px;
+    font-weight: 500;
+    color: #A9A9A9;
+`
+
+export const IconButton = styled.Image`
+    width: 20px;
+    height: 20px;
+`
+
+export const IconTouchableOpacity = styled.TouchableOpacity`
+    width: 30px;
+`
+
+//TouchableOpacity
