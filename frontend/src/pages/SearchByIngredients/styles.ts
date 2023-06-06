3cc42820afb78f4500518054053830c0df8d0056
Natan Maia <natanmaiaplays@outlook.com>
2023-06-06 10:50:33 -0300

    Feat: Create Styles to initialize SearchByIngredientes

---
+++ frontend/src/pages/SearchByIngredients/styles.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+    display: flex;
+    align-items: center;
+    width: 100%;
+    margin-top: 70px;
+    gap: 40px;
+`
+
+export const TitleText = styled.Text`
+    font-size: 30px;
+    font-weight: 700;
+    width: 220px;
+`
