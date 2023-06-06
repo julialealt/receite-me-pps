2ca473e1101eb2ee97acb2c01d8b5f05e77b90c8
Natan Maia <natanmaiaplays@outlook.com>
2023-06-06 10:47:23 -0300

    Feat: Create styles to ADdINgredientsButton

---
+++ frontend/src/components/AddIngredientsButton/styles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components/native';
+
+export const ScopeButton = styled.TouchableOpacity`
+    border-radius: 10px;
+    border-width: 1.2px;
+    border-color: #a9a9a9;
+`
+
+export const TextButton = styled.Text`
+    font-size: 16px;
+    font-weight: 500;
+    color: #a9a9a9;
+    padding: 10px 15px;
+`
