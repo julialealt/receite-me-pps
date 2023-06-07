b843fef179880d27e7435b74cbf395863db0d4ab
Gustavo <gust4v0309@gmail.com>
2023-06-07 01:18:15 -0300

    Feat: starting SeachByIngredients layout

--- frontend/src/pages/SearchByIngredients/index.tsx
+++ frontend/src/pages/SearchByIngredients/index.tsx
@@ -1,17 +1,53 @@
-import { ScrollView } from "react-native";
+import { ScrollView, View } from "react-native";
 import AddIngredientsButton from "../../components/AddIngredientsButton";
 import AddIngredients from "../../components/AddIngredientsButton";
 import SelectFilter from "../../components/SelectFilter";
-import { Container, TitleText } from "./styles";
+import { ButtonContainer, Container, IngredientsContainer, Test, TitleText } from "./styles";
+import SelectedIngredient from "../../components/SelectedIngredient";
+import Button from "../../components/Button";
+import { useState } from "react";
+
+interface Ingredient {
+    id: number;
+    ingredients: string;
+}
+
+const data: Ingredient[] = [
+    { id: 1, ingredients: "Farinha" },
+    { id: 2, ingredients: "Açúcar" },
+    { id: 3, ingredients: "Leite" },
+    { id: 4, ingredients: "Ovos" },
+    { id: 5, ingredients: "Manteiga" },
+    { id: 6, ingredients: "Fermento em pó" },
+    { id: 7, ingredients: "Sal" },
+    { id: 8, ingredients: "Chocolate" },
+    { id: 9, ingredients: "Canela" },
+    { id: 10, ingredients: "Óleo vegetal" },
+    { id: 11, ingredients: "Framboesas" },
+    { id: 12, ingredients: "Amêndoas" },
+    { id: 13, ingredients: "Baunilha" },
+    { id: 14, ingredients: "Limão" },
+    { id: 15, ingredients: "Coco ralado" },
+];
 
 export default function SearchByIngredients() {
+    const [addIngredient, setAddIngredient] = useState(false);
+
+
     return(
-        <Container>
-            <TitleText>Quais ingredientes você tem disponíveis ?</TitleText>
-            <AddIngredientsButton />
-            <ScrollView>
-                <SelectFilter />
-            </ScrollView>
-        </Container>
+        <ScrollView>
+            <Container>
+                <TitleText>Quais ingredientes você tem disponíveis ?</TitleText>
+                <AddIngredientsButton onPress={() => setAddIngredient(!addIngredient)} />
+                <SelectFilter show={addIngredient ? 'flex' : 'none'} />
+                <IngredientsContainer>
+                    <SelectedIngredient label="queijo" />
+                    <SelectedIngredient label="queijo" />
+                </IngredientsContainer>
+                <ButtonContainer>
+                    <Button labelButton="Pesquisar  >" height={70} width={240} radius={50}/>
+                </ButtonContainer>
+            </Container>
+        </ScrollView>
     )
 }
