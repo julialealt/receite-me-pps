42287c214ba7eb66f27f338a9d2b793acfda47a3
Gustavo <gust4v0309@gmail.com>
2023-06-08 20:32:53 -0300

    Feat: create styles to SelectIngredientButton

---
+++ frontend/src/components/SelectIngredientButton/styles.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components/native';
+
+export const ScopeButton = styled.View`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background: #22A36D;
+    padding: 10px 15px;
+    border-radius: 50px;
+`
+
+export const TitleButton = styled.Text`
+    font-size: 12px;
+    font-weight: 500;
+    color: #FFFFFF;
+`
