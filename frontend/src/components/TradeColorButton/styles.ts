664e98fcea814946d00c8f4490ff26886043c273
Gustavo <gust4v0309@gmail.com>
2023-06-12 19:53:13 -0300

    Feat: Create Styles to TradeColorButton

---
+++ frontend/src/components/TradeColorButton/styles.ts
@@ -0,0 +1,22 @@
+import styled from 'styled-components/native';
+
+interface ScopeButtonProps {
+    colorButton?: boolean
+  }
+
+export const ScopeButton = styled.TouchableOpacity<ScopeButtonProps>`
+    flex: 1;
+    display: flex;
+    align-items: center;
+    background: ${({ colorButton }: ScopeButtonProps) => (colorButton ? '#22A36D' : 'transparent')};
+    padding: 10px 10px;
+    border-radius: 12px;
+
+`
+
+export const TextButton = styled.Text<ScopeButtonProps>`
+    flex: 1;
+    font-size: 15px;
+    font-weight: 700;
+    color: ${({ colorButton }: ScopeButtonProps) => (colorButton ? '#FFFFFF' : '#A9A9A9')};
+`
