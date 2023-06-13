9b63447be239d836f730676960eb9db4da1872fc
Gustavo <gust4v0309@gmail.com>
2023-06-13 20:09:47 -0300

    Feat: Init base tp FavoriteBook Page

---
+++ frontend/src/pages/FavoriteBook/index.tsx
@@ -0,0 +1,27 @@
+import { ScrollView } from "react-native";
+import { BottomSideRightImage, Container, ContainerFavoriteBook, ContainerImages, LeftContainerImages, LeftImage, MainTitle, RightContainerImages, ScrollViewContainer, SeeMore, TitleContainer, TitleFavoriteBook, TopSideRightImage } from "./styles";
+
+export default function FavoriteBook() {
+    return(
+        <ScrollViewContainer>
+            <Container>
+                <MainTitle>Favoritos</MainTitle>
+                <ContainerFavoriteBook>
+                    <TitleContainer>
+                        <TitleFavoriteBook>Favoritos</TitleFavoriteBook>
+                        <SeeMore>Ver mais</SeeMore>
+                    </TitleContainer>
+                    <ContainerImages>
+                        <LeftContainerImages>
+                            <LeftImage source={require("../../assets/recipes/Ramen2.png")} />
+                        </LeftContainerImages>
+                        <RightContainerImages>
+                            <TopSideRightImage source={require("../../assets/recipes/Ramen2.png")} />
+                            <BottomSideRightImage source={require("../../assets/recipes/Ramen2.png")} />
+                        </RightContainerImages>
+                    </ContainerImages>
+                </ContainerFavoriteBook>
+            </Container>
+        </ScrollViewContainer>
+    )
+}
