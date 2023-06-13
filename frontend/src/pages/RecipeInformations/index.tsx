c87cd721a65f9af19968cc3cb64b1447ba22d89b
Gustavo <gust4v0309@gmail.com>
2023-06-13 00:42:29 -0300

    Feat: add two objects array in RecipeInformations

--- frontend/src/pages/RecipeInformations/index.tsx
+++ frontend/src/pages/RecipeInformations/index.tsx
@@ -1,5 +1,5 @@
-import { ScrollView } from "react-native";
-import { Container, ContainerButtons, ContainerFirstLayer, ContainerInformations, ContainerIngredients, ContainerMacroNutrients, ImageIcon, ImageRecipe, NumberMacroNutrients, SeparateIcons, SepareteMacroNutrients, TextFirstLayer, TextMacroNutrients, TitleInformations } from "./styles";
+import { ScrollView, Text } from "react-native";
+import { Container, ContainerButtons, ContainerFirstLayer, ContainerInformations, ContainerIngredientsAmounts, ContainerMacroNutrients, ContainerPreparationMethod, ImageIcon, ImageRecipe, IngredientsAmountsUnit, NumberMacroNutrients, PrepationMethodCircle, QuantityIngredientValue, SeparateIcons, SepareteMacroNutrients, TextFirstLayer, TextIngredientAmount, TextMacroNutrients, TextPrepationMethod, TitleInformations, UnitPrepationMethod } from "./styles";
 import TradeColorButton from "../../components/TradeColorButton";
 import { useState } from "react";
 
@@ -7,6 +7,12 @@ interface macroNutrients {
     id: Number,
     MacroNutrient: string,
     Quantity: Number
+}
+
+interface ingredients {
+    id: Number,
+    ingredient: string,
+    quantity: string
 }
 
 const macroNutrients: macroNutrients[] = [
@@ -26,6 +32,33 @@ const macroNutrients: macroNutrients[] = [
         Quantity: 13,
     },
 ]
+
+const ingredients: ingredients[] = [
+    { id: 1, ingredient: 'Costelinha suína', quantity: '200g' },
+    { id: 2, ingredient: 'Macarrão instantâneo', quantity: '1 unidade' },
+    { id: 3, ingredient: 'Acelga', quantity: 'a gosto' },
+    { id: 4, ingredient: 'Cebolinha', quantity: 'a gosto' },
+    { id: 5, ingredient: 'Cebola', quantity: '1 unidade' },
+    { id: 6, ingredient: 'Shoyu', quantity: '100ml' },
+    { id: 7, ingredient: 'Dente de alho', quantity: '4 unidades' },
+    { id: 8, ingredient: 'Sal', quantity: 'a gosto' },
+    { id: 9, ingredient: 'Gengibre', quantity: '1/6' },
+    { id: 10, ingredient: 'Água', quantity: '2L' },
+    { id: 11, ingredient: 'Milho', quantity: 'a gosto' },
+];
+
+const instructions: string[] = [
+    'Em uma panela, frite o alho moído em fogo baixo até dourar',
+    'Depois, coloque a costelinha na panela com a gordura virada para baixo até que doure',
+    'Vire e deixe dourar o outro lado também',
+    'Acrescente a água e deixe fervendo, depois abaixe o fogo',
+    'Misture o gengibre ralado no molho shoyu e coloque a mistura na panela',
+    'Coloque o macarrão e deixe cozinhar por 4 minutos, escorra e reserve',
+    'Coloque um pouco de água para ferver e jogue em cima da moyashi',
+    'Coloque o macarrão na tigela e acrescente 2 conchas do caldo do lámen',
+    'Em seguida, coloque as costelinhas, os ovos, a acelga, o moyashi e jogue mais caldo por cima',
+    'Jogue a cebolinha picada por cima e, pronto, bom apetite!'
+];
 
 export default function RecipeInformations() {
     const [tradeInformation, setTradeInformation] = useState(false)
@@ -59,9 +92,6 @@ export default function RecipeInformations() {
                         <TradeColorButton labelButton="Modo de preparo" color={!tradeInformation} onPress={() => setTradeInformation(false)} />
                     </ContainerButtons>
                 </ContainerInformations>
-                {/* <ContainerIngredients>
-
-                </ContainerIngredients> */}
             </Container>
         </ScrollView>
     )
