2f95acc8ca8ec301ee53ee2406b58d6edb33a952
Gustavo <gust4v0309@gmail.com>
2023-06-16 23:09:33 -0300

    Feat: create CustomScrollView to change backgroundColor to #FFFFFF

--- frontend/globalStyles.tsx
+++ frontend/globalStyles.tsx
@@ -1,5 +1,21 @@
 import styled from 'styled-components/native';
+import { ReactNode } from 'react';
+import { ScrollView, ScrollViewProps } from "react-native";
+
+type CustomScrollViewProps = ScrollViewProps & {
+    children: ReactNode;
+  };
 
 export const Weight700 = styled.Text`
     font-weight: 700;
 `
+
+export const CustomScrollView: React.FC<CustomScrollViewProps> = ({ children, ...props }) => {
+    return (
+        <ScrollView style={{ backgroundColor: "#FFFFFF" }} {...props}>
+        {children}
+        </ScrollView>
+    );
+};
+  
+  
