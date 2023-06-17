62c71c433e8e9210797c51cb5a0a9d0a68faaba1
Gustavo <gust4v0309@gmail.com>
2023-06-16 23:12:35 -0300

    Feat: add navigation in Login

--- frontend/src/pages/Login/index.tsx
+++ frontend/src/pages/Login/index.tsx
@@ -1,17 +1,18 @@
+import { useNavigation } from "@react-navigation/native";
 import { Weight700 } from "../../../globalStyles";
 import Button from "../../components/Button";
 import Input from "../../components/Input";
 import { Container, ContainerButton, LoginText, SubText } from "./styles";
 
 export default function Login() {
+    const navigation = useNavigation();
     return(
         <Container>
             <LoginText>Login</LoginText>
             <Input label="Email" placeholder="joao@gmail.com"/>
             <Input label="Senha" placeholder="*********"/>
             <ContainerButton>
-                <Button labelButton="ENTRAR" width={290} height={47} radius={10} />
-                <SubText>Esqueci minha senha</SubText>
+                <Button labelButton="ENTRAR" width={290} height={47} radius={10} onPress={() => navigation.navigate('mainViewer')} />
             </ContainerButton>
         </Container>
     )
