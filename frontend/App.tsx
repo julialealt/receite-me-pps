c30f86f6bb131ab64ba14340aabf5215be4073ca
Gustavo <gust4v0309@gmail.com>
2023-06-16 23:11:07 -0300

    Feat: add Routes in app.tsx

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,8 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import InitialSliders from './src/pages/InitialSliders';
-import Login from './src/pages/Login';
 import SignUp from './src/pages/SignUp';
+import Login from './src/pages/Login';
 import MainViewer from './src/pages/MainViewer';
 import SearchViewer from './src/pages/SearchViewer';
 import MainProfile from './src/pages/Profile/MainProfile';
@@ -14,9 +14,10 @@ import RecipesByIngredients from './src/pages/RecipesByIngredients';
 import RecipeInformations from './src/pages/RecipeInformations';
 import FavoriteBook from './src/pages/FavoriteBook';
 import BottomBar from './src/components/BottomBar';
+import { Routes } from './src/routes';
 
 export default function App() {
   return (
-    <BottomBar />
+    <Routes />
   );
 }
