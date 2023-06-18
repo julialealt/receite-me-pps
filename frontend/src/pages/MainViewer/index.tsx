02adb16e97c4cf8ced1a36bd050626391e513764
Gustavo <gust4v0309@gmail.com>
2023-06-18 17:54:45 -0300

    feat: add navigation MainViewer to recipeInformations

--- frontend/src/pages/MainViewer/index.tsx
+++ frontend/src/pages/MainViewer/index.tsx
@@ -4,6 +4,7 @@ import Search from "../../components/Search";
 import CategoryButton from "../../components/CategoryButton";
 import RecipeButton from "../../components/RecipeButton";
 import BottomBar from "../../components/BottomBar";
+import { useNavigation } from "@react-navigation/native";
 
 
 const categories = [
@@ -19,37 +20,20 @@ const categories = [
       image: require('../../assets/categories/Taco.png')}
   ];
   
-const recipes = [
-{ name: 'Coxinha', 
-  time: '10-15 min',
-  image: require('../../assets/recipes/Coxinha.png') },
-{ name: 'Feijoada', 
-  time: '10-15 min',
-  image: require('../../assets/recipes/Feijoada.png') },
-{ name: 'Filé Mignon', 
-  time: '10-15 min',
-  image: require('../../assets/cat-logo.png') },
-{ name: 'Hamburguer', 
-  time: '10-15 min',
-  image: require('../../assets/recipes/Hamburguer.png') },
-{ name: 'Cachorro Quente', 
-  time: '10-15 min',
-  image: require('../../assets/recipes/HotDog.png') },
-{ name: 'Ramen Coreano', 
-  time: '10-15 min',
-  image: require('../../assets/recipes/Ramen.png') },
-{ name: 'Spaghetti', 
-  time: '10-15 min',
-  image: require('../../assets/recipes/Spaghetti.png') },
-{ name: 'Sushi', 
-  time: '10-15 min',
-  image: require('../../assets/recipes/Sushi.png') },
-{ name: 'Yakissoba', 
-  time: '10-15 min',
-  image: require('../../assets/recipes/Yakissoba.png') },
-];
+  const recipes = [
+    { id: 1, name: 'Ramen Coreano', time: '10-15 min', image: require('../../assets/recipes/Ramen.png') },
+    { id: 2, name: 'Feijoada', time: '10-15 min', image: require('../../assets/recipes/Feijoada.png') },
+    { id: 3, name: 'Filé Mignon', time: '10-15 min', image: require('../../assets/cat-logo.png') },
+    { id: 4, name: 'Hamburguer', time: '10-15 min', image: require('../../assets/recipes/Hamburguer.png') },
+    { id: 5, name: 'Cachorro Quente', time: '10-15 min', image: require('../../assets/recipes/HotDog.png') },
+    { id: 6, name: 'Coxinha', time: '10-15 min', image: require('../../assets/recipes/Coxinha.png') },
+    { id: 7, name: 'Spaghetti', time: '10-15 min', image: require('../../assets/recipes/Spaghetti.png') },
+    { id: 8, name: 'Sushi', time: '10-15 min', image: require('../../assets/recipes/Sushi.png') },
+    { id: 9, name: 'Yakissoba', time: '10-15 min', image: require('../../assets/recipes/Yakissoba.png') },
+  ];
 
 export default function MainViewer() {
+    const navigation = useNavigation();
     return(
         <ScrollView>
             <Container>
@@ -75,8 +59,8 @@ export default function MainViewer() {
                     <TitleText>Recomendações</TitleText>
                     <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
                         <ScrollContainer>
-                            {recipes.map(({name, time, image}, index) => (
-                                <RecipeButton key={index} label={name} icon={image} time={time} onPress={() => console.log('test')} />
+                            {recipes.map(({ id ,name, time, image}) => (
+                                <RecipeButton key={id} label={name} icon={image} time={time} onPress={() => navigation.navigate("recipeInformations", {id: id})} />
                             ))}
                         </ScrollContainer>
                     </ScrollView>
@@ -85,8 +69,8 @@ export default function MainViewer() {
                     <TitleText>Receitas recentes</TitleText>
                     <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
                         <ScrollContainer>
-                            {recipes.map(({name, time, image}, index) => (
-                                <RecipeButton key={index} label={name} icon={image} time={time} onPress={() => console.log('test')} />
+                            {recipes.map(({ id ,name, time, image}) => (
+                                <RecipeButton key={id} label={name} icon={image} time={time} onPress={() => navigation.navigate("recipeInformations", {id: id})} />
                             ))}
                         </ScrollContainer>
                     </ScrollView>
@@ -95,8 +79,8 @@ export default function MainViewer() {
                     <TitleText>Receitas da semana</TitleText>
                     <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
                         <ScrollContainer>
-                            {recipes.map(({name, time, image}, index) => (
-                                <RecipeButton key={index} label={name} icon={image} time={time} onPress={() => console.log('test')} />
+                            {recipes.map(({ id ,name, time, image}) => (
+                                <RecipeButton key={id} label={name} icon={image} time={time} onPress={() => navigation.navigate("recipeInformations", {id: id})} />
                             ))}
                         </ScrollContainer>
                     </ScrollView>
