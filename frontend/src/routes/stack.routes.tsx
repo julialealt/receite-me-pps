e79d3435306e2bc586ecf5790d4ad86cba1047b1
Gustavo <gust4v0309@gmail.com>
2023-06-21 21:47:55 -0300

    Feat: Adding new Screen's in Stack.routes

--- frontend/src/routes/stack.routes.tsx
+++ frontend/src/routes/stack.routes.tsx
@@ -5,6 +5,9 @@ import Login from '../pages/Login';
 import MainViewer from '../pages/MainViewer';
 import RecipeInformations from '../pages/RecipeInformations';
 import CategoryRecipes from '../pages/CategoryRecipes';
+import SearchByIngredients from '../pages/SearchByIngredients';
+import MainProfile from '../pages/Profile/MainProfile';
+import FavoriteBook from '../pages/FavoriteBook';
 
 const { Screen, Navigator } = createNativeStackNavigator();
 
@@ -38,6 +41,18 @@ export default function StackRoutes() {
             <Screen
                 name='categoryRecipes'
                 component={CategoryRecipes}
+            />
+            <Screen
+                name='searchByIngredients'
+                component={SearchByIngredients}
+            />
+            <Screen
+                name='favoriteBook'
+                component={FavoriteBook}
+            />
+            <Screen
+                name='profile'
+                component={MainProfile}
             />
         </Navigator>
     )
