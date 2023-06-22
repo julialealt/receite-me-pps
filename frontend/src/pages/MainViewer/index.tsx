9bba7444b7662cefb62118cd89d42b0ee823e8c6
Gustavo <gust4v0309@gmail.com>
2023-06-21 22:43:00 -0300

    Feat: lembro n tlgd

--- frontend/src/pages/MainViewer/index.tsx
+++ frontend/src/pages/MainViewer/index.tsx
@@ -1,4 +1,4 @@
-import { Container, GreetingText, GreetingUserContainer, ScrollCategoryContainer, ScrollContainer, TextContainer, TitleText, UserContainer, UserLogo } from "./styles";
+import { BottomBarContainer, Container, GreetingText, GreetingUserContainer, ScrollCategoryContainer, ScrollContainer, TextContainer, TitleText, UserContainer, UserLogo } from "./styles";
 import {CustomScrollView as ScrollView } from "../../../globalStyles";
 import Search from "../../components/Search";
 import CategoryButton from "../../components/CategoryButton";
@@ -7,6 +7,7 @@ import BottomBar from "../../components/BottomBar";
 import { useNavigation } from "@react-navigation/native";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import { View } from "react-native";
 
 const categories = [
     { name: 'Vegano', 
@@ -62,62 +63,62 @@ export default function MainViewer() {
     useEffect(() => {
         getRecipeInformations();
     }, [])
-    console.log(recipeData)
-    // console.log(recipeData && recipeData[0] && `../../assets/recipes/${recipeData[0].image}`);
 
     return(
-        <ScrollView>
-            <Container>
-                <UserContainer>
-                    <GreetingUserContainer>
-                        <GreetingText>Olá, Rossana</GreetingText>
-                        <TitleText>Vamos Cozinhar uma boa receita hoje!</TitleText>
-                    </GreetingUserContainer>
-                    <UserLogo source={require('../../assets/Users/Janet.png')} />
-                </UserContainer>
-                <Search placeholder="Pesquise por receitas" />
-                <TextContainer>
-                    <TitleText>Categorias</TitleText>
-                    <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
-                        <ScrollCategoryContainer>
-                        {categories.map(({ name, image }, index) => (
-                                <CategoryButton key={index} label={name} Icon={image} onPress={() => navigation.navigate('categoryRecipes', { category: name })} />
-                            ))}
-                        </ScrollCategoryContainer>
-                    </ScrollView>
-                </TextContainer>
-                <TextContainer>
-                    <TitleText>Recomendações</TitleText>
-                    <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
-                        <ScrollContainer>
-                            {recipeData?.map(({ id ,name, time, image}) => (
-                                <RecipeButton key={id} label={name} icon={require('../../assets/recipes/Ramen2.png')} time={time} onPress={() => navigation.navigate("recipeInformations", {id: id})} />
-                            ))}
-                        </ScrollContainer>
-                    </ScrollView>
-                </TextContainer>
-                <TextContainer>
-                    <TitleText>Receitas recentes</TitleText>
-                    <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
-                        <ScrollContainer>
-                            {recipeData?.map(({ id ,name, time, image}) => (
-                                <RecipeButton key={id} label={name} icon={require('../../assets/recipes/Ramen2.png')} time={time} onPress={() => navigation.navigate("recipeInformations", {id: id})} />
-                            ))}
-                        </ScrollContainer>
-                    </ScrollView>
-                </TextContainer>
-                <TextContainer>
-                    <TitleText>Receitas da semana</TitleText>
-                    <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
-                        <ScrollContainer>
-                            {recipeData?.map(({ id ,name, time, image}) => (
-                                <RecipeButton key={id} label={name} icon={require('../../assets/recipes/Ramen2.png')} time={time} onPress={() => navigation.navigate("recipeInformations", {id: id})} />
-                            ))}
-                        </ScrollContainer>
-                    </ScrollView>
-                </TextContainer>
-            </Container>
+        <View>
+            <ScrollView>
+                <Container>
+                    <UserContainer>
+                        <GreetingUserContainer>
+                            <GreetingText>Olá, Rossana</GreetingText>
+                            <TitleText>Vamos Cozinhar uma boa receita hoje!</TitleText>
+                        </GreetingUserContainer>
+                        <UserLogo source={require('../../assets/Users/Janet.png')} />
+                    </UserContainer>
+                    <Search placeholder="Pesquise por receitas" />
+                    <TextContainer>
+                        <TitleText>Categorias</TitleText>
+                        <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
+                            <ScrollCategoryContainer>
+                            {categories.map(({ name, image }, index) => (
+                                    <CategoryButton key={index} label={name} Icon={image} onPress={() => navigation.navigate('categoryRecipes', { category: name })} />
+                                ))}
+                            </ScrollCategoryContainer>
+                        </ScrollView>
+                    </TextContainer>
+                    <TextContainer>
+                        <TitleText>Recomendações</TitleText>
+                        <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
+                            <ScrollContainer>
+                                {recipeData?.map(({ id ,name, time, image}) => (
+                                    <RecipeButton key={id} label={name} icon={image} time={time} onPress={() => navigation.navigate("recipeInformations", {id: id})} />
+                                ))}
+                            </ScrollContainer>
+                        </ScrollView>
+                    </TextContainer>
+                    <TextContainer>
+                        <TitleText>Receitas recentes</TitleText>
+                        <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
+                            <ScrollContainer>
+                                {recipeData?.map(({ id ,name, time, image}) => (
+                                    <RecipeButton key={id} label={name} icon={image} time={time} onPress={() => navigation.navigate("recipeInformations", {id: id})} />
+                                ))}
+                            </ScrollContainer>
+                        </ScrollView>
+                    </TextContainer>
+                    <TextContainer>
+                        <TitleText>Receitas da semana</TitleText>
+                        <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
+                            <ScrollContainer>
+                                {recipeData?.map(({ id ,name, time, image}) => (
+                                    <RecipeButton key={id} label={name} icon={image} time={time} onPress={() => navigation.navigate("recipeInformations", {id: id})} />
+                                ))}
+                            </ScrollContainer>
+                        </ScrollView>
+                    </TextContainer>
+                </Container>
+            </ScrollView>
             <BottomBar />
-        </ScrollView>
+        </View>
     )
 }
