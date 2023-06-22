bc36ff75dec78ace430dac3dc2f59395264753f3
Gustavo <gust4v0309@gmail.com>
2023-06-21 22:44:08 -0300

    Feat: upgrade BottomBar

--- frontend/src/components/BottomBar/index.tsx
+++ frontend/src/components/BottomBar/index.tsx
@@ -1,20 +1,54 @@
 import { TouchableOpacity } from "react-native";
 import { BottomBarIcon, Container } from "./styles";
+import { useNavigation } from "@react-navigation/native";
 
-const Icon = [
-    require("../../assets/bottomBar/home.png"),
-    require("../../assets/bottomBar/magnifier.png"),
-    require("../../assets/bottomBar/heart.png"),
-    require("../../assets/bottomBar/profile.png"),
+interface IconProps {
+    id: number,
+    image: any,
+    navigation: () => void
+}
 
-]
 
 export default function BottomBar() {
+    const navigation = useNavigation();
+
+    const Icon: IconProps[] = [
+        {
+            id: 1,
+            image: require("../../assets/bottomBar/home.png"),
+            navigation: () => navigation.navigate("mainViewer")
+    
+        },
+        {
+            id: 2,
+            image: require("../../assets/bottomBar/magnifier.png"),
+            navigation: () => navigation.navigate("searchByIngredients")
+
+    
+        
+        },
+        {
+            id: 3,
+            image: require("../../assets/bottomBar/heart.png"),
+            navigation: () => navigation.navigate("favoriteBook")
+
+    
+        
+        },
+        {
+            id: 4,
+            image: require("../../assets/bottomBar/profile.png"),
+            navigation: () => navigation.navigate("profile")
+        
+        },
+        
+    ]
+
     return(
-        <Container>
-            {Icon.map((item, index) =>(
-                <TouchableOpacity key={index}>
-                    <BottomBarIcon source={item} />
+        <Container style={{position: 'absolute', left: 0, right: 0, bottom: 0}}>
+            {Icon.map(({id,image, navigation}) =>(
+                <TouchableOpacity key={id} onPress={navigation}>
+                    <BottomBarIcon source={image} />
                 </TouchableOpacity>
             ))}
         </Container>
