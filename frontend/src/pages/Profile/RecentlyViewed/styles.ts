a809e06ff41b79b35f8d6c186b0c41113cb5fb01
Gustavo <gust4v0309@gmail.com>
2023-06-22 18:35:15 -0300

    Feat: create new styles to RecentlyViewed

--- frontend/src/pages/Profile/RecentlyViewed/styles.ts
+++ frontend/src/pages/Profile/RecentlyViewed/styles.ts
@@ -4,7 +4,8 @@ export const Container = styled.View `
     display: flex;
     flex-direction: column;
     align-items: center;
-    margin-top: 85px;
+    padding-top: 85px;
+    padding: 40px 0;
     width: 100%;
 `
 
@@ -23,3 +24,17 @@ export const RecipeContainer = styled.View`
     gap: 20px;
     margin-top: 25px;
 `;
+
+export const BackThePage = styled.View`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0 20px;
+    width: 100%;
+`;
+
+export const BackArrow = styled.Image`
+    width: 25px;
+    height: 25px;
+`
