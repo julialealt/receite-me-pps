e5bdf24f890a5153bd1afd332038ecc38fa4a49f
Gustavo <gust4v0309@gmail.com>
2023-06-22 16:10:56 -0300

    Feat: add navigation in MainProfile

--- frontend/src/pages/Profile/MainProfile/index.tsx
+++ frontend/src/pages/Profile/MainProfile/index.tsx
@@ -2,21 +2,56 @@ import { View } from "react-native";
 import {CustomScrollView as ScrollView } from "../../../../globalStyles";
 import BottomBar from "../../../components/BottomBar";
 import { Container, UserContainer, UserFunctionsIconContainer, UserFunctionsList, UserFunctionsText, UserIcon, UserLogo, UserName, UserStatus } from "./styles";
+import { useNavigation } from "@react-navigation/native";
 
-const profiles = [
-    { text: "Editar Perfil", 
-      icon: require("../../../assets/profileIcons/Pencil.png") },
-    { text: "Favoritos", 
-      icon: require("../../../assets/profileIcons/Favorite.png") },
-    { text: "Recentemente Visualizadas", 
-      icon: require("../../../assets/profileIcons/Document.png") },
-    { text: "Logout", 
-      icon: require("../../../assets/profileIcons/Logout.png") },
-    { text: "Apagar Conta", 
-      icon: require("../../../assets/profileIcons/bin.png") },
-  ];
+interface ProfileProps {
+  id: number,
+  text: string
+  icon: any,
+  navigation: () => void
+}
 
 export default function MainProfile() {
+    const navigation = useNavigation();
+
+    const profiles: ProfileProps[] = [
+      { 
+        id: 1,
+        text: "Editar Perfil", 
+        icon: require("../../../assets/profileIcons/Pencil.png"),
+        navigation: () => navigation.navigate("editProfile") 
+
+      },
+      { 
+        id: 2,
+        text: "Favoritos", 
+        icon: require("../../../assets/profileIcons/Favorite.png"),
+        navigation: () => navigation.navigate("favoriteBook") 
+
+      },
+      { 
+        id: 3,
+        text: "Recentemente Visualizadas", 
+        icon: require("../../../assets/profileIcons/Document.png"),
+        navigation: () => navigation.navigate("recentlyViewed") 
+
+      },
+      { 
+        id: 4,
+        text: "Logout", 
+        icon: require("../../../assets/profileIcons/Logout.png"),
+        navigation: () => navigation.navigate("favoriteBook") 
+
+      },
+      { 
+        id: 5,
+        text: "Apagar Conta", 
+        icon: require("../../../assets/profileIcons/bin.png"),
+        navigation: () => navigation.navigate("favoriteBook") 
+
+      },
+    ];
+    
     return(
       <View>
         <ScrollView>
@@ -27,8 +62,8 @@ export default function MainProfile() {
                     <UserStatus>Apaixonada por não morrer de fome</UserStatus>
                 </UserContainer>
                 <UserFunctionsList>
-                    {profiles.map(({text, icon}, index) => (
-                        <UserFunctionsIconContainer key={index}>
+                    {profiles.map(({id, icon, text, navigation}) => (
+                        <UserFunctionsIconContainer onPress={navigation} key={id}>
                             <UserIcon source={icon} />
                             <UserFunctionsText>{text}</UserFunctionsText>
                         </UserFunctionsIconContainer>
