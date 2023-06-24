dcdc604385906abce0ceac43f6a9d7b353a5f1e8
Júlia Leal <juliateixeira@gotest.com.br>
2023-06-24 14:26:54 -0300

    tab navigator finished

--- frontend/src/newRoutes/tabs.routes.tsx
+++ frontend/src/newRoutes/tabs.routes.tsx
@@ -1,9 +1,13 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-import HomeSvg from "../assets/tabNavigation/GreenHome.svg"
-import SearchSvg from "../assets/tabNavigation/BlackSearch.svg"
-import FavoritesSvg from "../assets/tabNavigation/BlackHeart.svg"
-import ProfileSvg from "../assets/tabNavigation/BlackProfile.svg"
+import GreenHomeSvg from "../assets/tabNavigation/GreenHome.svg"
+import BlackHomeSvg from "../assets/tabNavigation/BlackHome.svg"
+import GreenSearchSvg from "../assets/tabNavigation/GreenSearch.svg"
+import BlackSearchSvg from "../assets/tabNavigation/BlackSearch.svg"
+import GreenHeartSvg from "../assets/tabNavigation/GreenHeart.svg"
+import BlackHeartSvg from "../assets/tabNavigation/BlackHeart.svg"
+import GreenProfileSvg from "../assets/tabNavigation/GreenProfile.svg"
+import BlackProfileSvg from "../assets/tabNavigation/BlackProfile.svg"
 import { HomeRoutes } from "./home.routes";
 import SearchByIngredients from "../pages/SearchByIngredients";
 import Favorites from "../pages/FavoriteBook";
@@ -17,12 +21,25 @@ export function TabsRoutes() {
     return (
         <Navigator screenOptions={{ 
             headerShown: false, tabBarShowLabel: false,
+            tabBarStyle: {
+                position: "absolute",
+                backgroundColor: "#FFFFFF",
+                borderTopWidth: 0,
+                borderTopLeftRadius: 15,
+                borderTopRightRadius: 15,
+                height: 80
+            }
          }}>
             <Screen 
                 name="homeRoutes" 
                 component={HomeRoutes}
                 options={{ 
-                    tabBarIcon: () => <HomeSvg/>
+                    tabBarIcon: ({ focused }) => {
+                        if (focused) {
+                            return <GreenHomeSvg/>
+                        }
+                        return <BlackHomeSvg/>
+                    }
                 }}
             />
 
@@ -30,7 +47,12 @@ export function TabsRoutes() {
                 name="search"  
                 component={SearchByIngredients}
                 options={{
-                    tabBarIcon: () => <SearchSvg/>
+                    tabBarIcon: ({ focused }) => {
+                        if (focused) {
+                            return <GreenSearchSvg/>
+                        }
+                        return <BlackSearchSvg/>
+                    }
                 }}
             />
 
@@ -38,7 +60,12 @@ export function TabsRoutes() {
                 name="favorites" 
                 component={Favorites}
                 options={{
-                    tabBarIcon: () => <FavoritesSvg/>
+                    tabBarIcon: ({ focused }) => {
+                        if (focused) {
+                            return <GreenHeartSvg/>
+                        }
+                        return <BlackHeartSvg/>
+                    }
                 }}
             />
 
@@ -46,7 +73,12 @@ export function TabsRoutes() {
                 name="profileRoutes" 
                 component={ProfileRoutes}
                 options={{
-                    tabBarIcon: () => <ProfileSvg/>
+                    tabBarIcon: ({ focused }) => {
+                        if (focused) {
+                            return <GreenProfileSvg/>
+                        }
+                        return <BlackProfileSvg/>
+                    }
                 }}
             />
 
