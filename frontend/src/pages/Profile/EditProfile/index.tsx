407d0c2d536bb3bde1d039436f8465b58f8e14e9
Gustavo <gust4v0309@gmail.com>
2023-06-25 16:25:34 -0300

    Feat: Starting integrate EditProfile

--- frontend/src/pages/Profile/EditProfile/index.tsx
+++ frontend/src/pages/Profile/EditProfile/index.tsx
@@ -4,9 +4,34 @@ import { BackArrow, BackThePage, Container, ContainerEditProfileInputs, EditProf
 import Button from "../../../components/Button";
 import { useNavigation } from "@react-navigation/native";
 import { Text, TouchableOpacity, View } from "react-native";
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../../../context/auth";
+
+interface FormData {
+    nome: string;
+    bio: string;
+    email: string;
+}
 
 export default function EditProfile() {
     const navigation = useNavigation()
+    const { data } = useContext(AuthContext)
+    const [formData, setFormData] = useState({
+        nome: data.name,
+        bio: data.bio,
+        email: data.email,
+    })
+
+    const handleInputChange = (name: keyof FormData, value: string) => {
+        setFormData(prevState => ({
+            ...prevState,
+            [name]: value,
+          }));
+    }
+
+    useEffect(() => {
+        console.log(formData)
+    }, [formData])
 
     return(
         <ScrollView>
@@ -19,10 +44,25 @@ export default function EditProfile() {
                     <Text>‎ ‎ ‎ ‎ </Text>
                 </BackThePage>
                 <ContainerEditProfileInputs>
-                    <Input label="Nome" placeholder="Rossana Andrade" />
-                    <Input label="Bio" placeholder="Alguma bio ou status" />
-                    <Input label="Email" placeholder="rossanaandrade@gmail.com" />
-                    <Input label="Senha tual" />
+                    <Input
+                        label="Nome"
+                        placeholder="Rossana Andrade"
+                        value={formData.nome}
+                        onChangeText={value => handleInputChange('nome', value)}
+                    />
+                    <Input
+                        label="Bio"
+                        placeholder="Alguma bio ou status"
+                        value={formData.bio}
+                        onChangeText={value => handleInputChange('bio', value)}
+                    />
+                    <Input
+                        label="Email"
+                        placeholder="rossanaandrade@gmail.com"
+                        value={formData.email}
+                        onChangeText={value => handleInputChange('email', value)}
+                    />
+                    <Input label="Senha atual" />
                     <Input label="Nova senha" />
                     <Input label="confirme nova senha" />
                 </ContainerEditProfileInputs>
