04caca8223aa2459f5534a832cb00bee63d3345a
Gustavo <gust4v0309@gmail.com>
2023-06-27 22:48:00 -0300

    Feat: finishing RecentlyViewed Screen

--- frontend/src/pages/Profile/RecentlyViewed/index.tsx
+++ frontend/src/pages/Profile/RecentlyViewed/index.tsx
@@ -5,46 +5,52 @@ import { Text, TouchableOpacity } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 
 import { propsStack } from '../../../routes/Models';
-import axios from "axios";
+import axios, { all } from "axios";
 import { useEffect, useState } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 interface RecipeData {
   id: number;    
-  name: string;
-  time: string;
-  image: string;
+  nome: string;
+  tempoDePreparo: number;
+  pathImagem: string;
 }
 
 export default function RecentlyViewed() {
     const navigation = useNavigation<propsStack>()
     const [allRecipes, setAllRecipes] = useState<RecipeData[] | null>([{ 
       id: 0, 
-      name: '', 
-      time: '', 
-      image: '' 
+      nome: '', 
+      tempoDePreparo: 0, 
+      pathImagem: '' 
     }]);
 
     const getRecipeInformations = async () => {
       try {
-          const response = await axios.get<RecipeData[]>('https://json-test-phi.vercel.app/recipes');
-          const updatedRecipeData = response.data.map(({id, image, name, time}) => {
-          if(name.length >= 14) {
-              name = name.slice(0, 13) + '...';
+        const recentlyViewedArray = await AsyncStorage.getItem("recentlyViewed");
+        const ids = JSON.parse(recentlyViewedArray || "[]");
+        const recipePromises = ids.map(async (id: number) => {
+          const response = await axios.get<RecipeData>(`https://c708-2804-d4b-9488-2d00-a190-6452-3398-36ba.ngrok-free.app/receitas/findById/${id}`);
+          const { nome, pathImagem, tempoDePreparo} = response.data;
+          let updatedName = nome;
+          if (nome.length >= 14) {
+            updatedName = nome.slice(0, 13) + '...';
           }
-
+          
           return {
-              id: id,
-              name: name,
-              time: time,
-              image: image
-          }
-          })
-          setAllRecipes(updatedRecipeData);
-
+            id: id,
+            nome: updatedName,
+            tempoDePreparo: tempoDePreparo,
+            pathImagem: pathImagem
+          };
+        });
+        
+        const updatedRecipeData = await Promise.all(recipePromises);
+        setAllRecipes(updatedRecipeData);
       } catch (error) {
-          console.error('Error fetching recipe data:', error);
+        console.error('Error fetching recipe data:', error);
       }
-    }
+    };
 
     useEffect(() => {
       getRecipeInformations()
@@ -61,8 +67,8 @@ export default function RecentlyViewed() {
             <Text>‎ ‎ ‎ ‎ ‎ </Text>
           </BackThePage>
             <RecipeContainer>
-              {allRecipes?.map(({name, time, image,id}) => (
-                  <RecipeButton key={id} label={name} icon={image} time={time} size="bigger" onPress={() => navigation.navigate("RecipeInformations", { id: id })} />
+              {allRecipes?.map(({id, nome, pathImagem, tempoDePreparo}) => (
+                  <RecipeButton key={id} label={nome} icon={pathImagem} time={tempoDePreparo} size="bigger" onPress={() => navigation.navigate("RecipeInformations", { id: id })} />
               ))}
             </RecipeContainer>
         </Container>
