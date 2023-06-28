7e85a8e58d6b3978994c2740d323c371cbde2698
Gustavo <gust4v0309@gmail.com>
2023-06-28 20:23:05 -0300

    Feat: integrating CategoryRecipes

--- frontend/src/pages/CategoryRecipes/index.tsx
+++ frontend/src/pages/CategoryRecipes/index.tsx
@@ -6,6 +6,7 @@ import { Text, TouchableOpacity } from "react-native";
 import { propsStack } from '../../routes/Models';
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { apiURL } from "../../../api";
 
 type ParamsProps = {
     category: string;
@@ -13,9 +14,9 @@ type ParamsProps = {
 
 interface RecipeData {
   id: number;    
-  name: string;
-  time: number;
-  image: string;
+  nome: string;
+  tempoDePreparo: number;
+  pathImagem: string;
 }
 
 export default function CategoryRecipes() {
@@ -24,32 +25,33 @@ export default function CategoryRecipes() {
     const { category } = route.params as ParamsProps;
     const [allRecipes, setAllRecipes] = useState<RecipeData[] | null>([{ 
       id: 0, 
-      name: '', 
-      time: 0, 
-      image: '' 
+      nome: '', 
+      tempoDePreparo: 0, 
+      pathImagem: '' 
     }]);
 
     const getRecipeInformations = async () => {
       try {
-          const response = await axios.get<RecipeData[]>('https://json-test-phi.vercel.app/recipes');
-          const updatedRecipeData = response.data.map(({id, image, name, time}) => {
-          if(name.length >= 14) {
-              name = name.slice(0, 13) + '...';
+        //feito
+        const response = await axios.get<RecipeData[]>(`${apiURL}/receitas/filtro/${category}`);
+        const updatedRecipeData = response.data.map(({ id, pathImagem, nome, tempoDePreparo }) => {
+          if (nome.length >= 14) {
+            nome = nome.slice(0, 13) + '...';
           }
-
           return {
-              id: id,
-              name: name,
-              time: time,
-              image: image
-          }
-          })
-          setAllRecipes(updatedRecipeData);
+            id: id,
+            nome: nome,
+            tempoDePreparo: tempoDePreparo,
+            pathImagem: pathImagem,
+          };
+        });
 
+        console.log(updatedRecipeData)
+        setAllRecipes(updatedRecipeData);
       } catch (error) {
-          console.error('Error fetching recipe data:', error);
+        console.error('Error fetching recipe data:', error);
       }
-    }
+    };
 
     useEffect(() => {
       getRecipeInformations()
@@ -66,8 +68,8 @@ export default function CategoryRecipes() {
             <Text>‎ ‎ ‎ ‎ </Text>
           </BackThePage>
           <RecipeContainer>
-            {allRecipes?.map(({name, time, image, id}) => (
-                <RecipeButton key={id} label={name} icon={image} time={time} size="bigger" onPress={() => navigation.navigate("RecipeInformations", { id: id })} />
+            {allRecipes?.map(({nome, id, pathImagem, tempoDePreparo}) => (
+                <RecipeButton key={id} label={nome} icon={pathImagem} time={tempoDePreparo} size="bigger" onPress={() => navigation.navigate("RecipeInformations", { id: id })} />
             ))}
           </RecipeContainer>
         </Container>
