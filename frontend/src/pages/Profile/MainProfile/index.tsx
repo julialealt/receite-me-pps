74bcfd53558919bd5391d54c672b5e5ec6fe3d4b
Gustavo <gust4v0309@gmail.com>
2023-07-01 17:58:53 -0300

    Feat: Finish MainProfile

--- frontend/src/pages/Profile/MainProfile/index.tsx
+++ frontend/src/pages/Profile/MainProfile/index.tsx
@@ -7,56 +7,72 @@ import { AuthContext } from "../../../context/auth";
 import { Ionicons } from '@expo/vector-icons';
 
 import { propsStack } from '../../../routes/Models';
+import axios from "axios";
+import { apiURL } from "../../../../api";
 
 interface ProfileProps {
   id: number,
   text: string,
   icon: any,
-  navigation: () => void
+  command: any
 }
 
 export default function MainProfile() {
     const navigation = useNavigation<propsStack>();
-    const { data } = useContext(AuthContext)
+    const { data, clearFormData } = useContext(AuthContext)
 
     const profiles: ProfileProps[] = [
       { 
         id: 1,
         text: "Editar perfil", 
         icon: "md-pencil-sharp",
-        navigation: () => navigation.navigate("EditProfile") 
+        command: () => navigation.navigate("EditProfile") 
 
       },
       { 
         id: 2,
         text: "Favoritos", 
         icon: "heart-outline",
-        navigation: () => navigation.navigate("Favorites") 
+        command: () => navigation.navigate("Favorites") 
 
       },
       { 
         id: 3,
         text: "Recentemente visualizadas", 
         icon: "ios-newspaper-outline",
-        navigation: () => navigation.navigate("RecentlyViewed") 
+        command: () => navigation.navigate("RecentlyViewed") 
 
       },
       { 
         id: 4,
         text: "Logout", 
         icon: "log-out-outline",
-        navigation: () => navigation.navigate("SignUp") 
+        command: () => handleLogout()
 
       },
       { 
         id: 5,
         text: "Excluir conta", 
         icon: "ios-trash-outline",
-        navigation: () => navigation.navigate("SignUp") 
+        command: () => handleDeleteUser(data.id)
 
       },
     ];
     
+    const handleLogout = () => {
+      clearFormData();
+      navigation.navigate("Login");
+    }
+
+    const handleDeleteUser = async (id: number) => {
+      try {
+        const response = await axios.delete(`${apiURL}/usuarios/${id}`);
+        navigation.navigate("SignUp");
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
     return(
       <View>
         <ScrollView>
@@ -70,8 +86,8 @@ export default function MainProfile() {
                     <UserStatus>{data.bio}</UserStatus>
                 </UserContainer>
                 <UserFunctionsList>
-                    {profiles.map(({id, icon, text, navigation}) => (
-                        <UserFunctionsIconContainer onPress={navigation} key={id}>
+                    {profiles.map(({id, icon, text, command}) => (
+                        <UserFunctionsIconContainer onPress={command} key={id}>
                             <Ionicons name={icon} size={22} color="black" />
                             <UserFunctionsText>{text}</UserFunctionsText>
                         </UserFunctionsIconContainer>
