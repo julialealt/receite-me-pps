058038a2cdcbf1e64328e6d5fed2f46c918ca3e0
Gustavo <gust4v0309@gmail.com>
2023-07-01 14:12:39 -0300

    Feat: Finishing SignUp

--- frontend/src/pages/SignUp/index.tsx
+++ frontend/src/pages/SignUp/index.tsx
@@ -12,67 +12,76 @@ import { apiURL } from "../../../api";
 interface FormData {
     nome: string;
     email: string;
-    password: string;
+    bio: string;
+    senha: string;
   }
 
 export default function SignUp() {
-    const navigation = useNavigation<propsStack>()
+  const navigation = useNavigation<propsStack>()
 
-    const [formData, setFormData] = useState<FormData>({
-        nome: '',
-        email: '',
-        password: '',
-    });
-    const [error, setError] = useState("");
-    const [success, setSuccess] = useState(false);
+  const [formData, setFormData] = useState<FormData>({
+      nome: '',
+      email: '',
+      bio: "Receite.me, o melhor app de receitas 👆🏻",
+      senha: '',
+  });
+  const [error, setError] = useState("");
+  const [success, setSuccess] = useState(false);
 
-    const handleInputChange = (name: keyof FormData, value: string) => {
-        setFormData(prevState => ({
-            ...prevState,
-            [name]: value,
-          }));
-    }
+  const handleInputChange = (name: keyof FormData, value: string) => {
+      setFormData(prevState => ({
+          ...prevState,
+          [name]: value,
+        }));
+  }
 
-    const handleSignUp = () => {
-        axios
-          .post(`${apiURL}/usuario`, formData)
-          .then((response) => {
-            console.log(response.data);
-            setSuccess(true);
+  const handleSignUp = () => {
+    if(!(formData.nome.length == 0 || formData.email.length == 0 || formData.senha.length == 0)) {
+      axios
+        .post(`${apiURL}/usuarios`, formData)
+        .then((response) => {
+          setSuccess(true);
+          setFormData({
+            nome: '',
+            email: '',
+            bio: '',
+            senha: '',
           })
-          .catch((error) => {
-            console.log(error);
-            setError("Erro ao cadastrar. Verifique os dados e tente novamente.");
-          });
-      };
-    
-      useEffect(() => {
-        if (success) {
-          navigation.navigate("Login");
-        }
-      }, [success, navigation]);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    }
+  };
+  
+  useEffect(() => {
+    if (success) {
+      navigation.navigate("Login");
+      setSuccess(false);
+    }
+  }, [success, navigation]);
 
-    return(
-        <Container>
-            <LoginText>Sign up</LoginText>
-            <Input label="Nome" type="text" placeholder="João" 
-                value={formData.nome}
-                onChangeText={value => handleInputChange('nome', value)}
-            />
-            <Input label="Email" type="email" placeholder="joao@gmail.com"
-                value={formData.email}
-                onChangeText={value => handleInputChange('email', value)}
-            />
-            <Input label="Senha" type="password" placeholder="*********"
-                value={formData.password}
-                onChangeText={value => handleInputChange('password', value)}
-            />
-            <ContainerButton>
-                <Button labelButton="CADASTRAR" onPress={handleSignUp} width={200} height={47} radius={10} />
-                <SubText>Já possui uma conta? <Weight700 
-                style={{color: "#22A36D"}} 
-                onPress={() => navigation.navigate('Login')}>Entre agora</Weight700></SubText>
-            </ContainerButton>
-        </Container>
-    )
+  return(
+      <Container>
+          <LoginText>Sign up</LoginText>
+          <Input label="Nome" type="text" placeholder="João" 
+              value={formData.nome}
+              onChangeText={value => handleInputChange('nome', value)}
+          />
+          <Input label="Email" keyboardType="email-address" placeholder="joao@gmail.com"
+              value={formData.email}
+              onChangeText={value => handleInputChange('email', value)}
+          />
+          <Input label="Senha" type="password" placeholder="*********"
+              value={formData.senha}
+              onChangeText={value => handleInputChange('senha', value)}
+          />
+          <ContainerButton>
+              <Button labelButton="CADASTRAR" onPress={handleSignUp} width={200} height={47} radius={10} />
+              <SubText>Já possui uma conta? <Weight700 
+              style={{color: "#22A36D"}} 
+              onPress={() => navigation.navigate('Login')}>Entre agora</Weight700></SubText>
+          </ContainerButton>
+      </Container>
+  )
 }
